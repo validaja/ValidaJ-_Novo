bf1ae7a1455404d6a9fab73a18c1a4b286b9dbb5
validaja <61952018+validaja@users.noreply.github.com>
2020-05-11 19:30:09 -0300

    Login

---
+++ conexao.php
@@ -0,0 +1,11 @@
+<?php
+
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$dbname = "projeto";
+
+//criando conexao
+$conexao = mysqli_connect($servidor, $usuario, $senha, $dbname);
+
+?>
